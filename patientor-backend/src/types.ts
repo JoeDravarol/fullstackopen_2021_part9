1ab689fe3e0194ce8c0694db32ecd7b8324969f4
Joe <joedravarol@gmail.com>
2021-07-03 13:01:14 +0100

    Complete patientor backend exercise step4 feat: Create an endpoint for fetching all patients.

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -3,3 +3,16 @@ export interface DiagnoseEntry {
   name: string;
   latin?: string;
 }
+
+export type Gender = 'male' | 'female';
+
+export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
+
+export interface PatientEntry {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: Gender;
+  occupation: string;
+}
