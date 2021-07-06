e27445f9a11faa4ef0cebd24ff102ec3d9a96a3e
Joe <joedravarol@gmail.com>
2021-07-06 05:30:52 +0100

    Complete patientor exercise step2 feat: Implement a view for displaying patient's information.

---
+++ patientor/src/PatientInfoPage/index.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
+import axios from 'axios';
+
+import { apiBaseUrl } from '../constants';
+import { Patient } from "../types";
+
+import GenderIcon from '../components/GenderIcon';
+import { useStateValue } from '../state';
+
+const PatientInfoPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [{ patient }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: 'SET_PATIENT', payload: data });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    if (!patient || patient.id !== id) {
+      void fetchPatient();
+    }
+  }, [id, dispatch]);
+
+  if (!patient) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>
+        {patient.name}
+        <GenderIcon gender={patient.gender} />
+      </h2>
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+    </div>
+  );
+};
+
+export default PatientInfoPage;
